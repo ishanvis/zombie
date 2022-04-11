e2fead462ad5bbe149d0321327efd49dbcd63209
ishanvis <94941577+ishanvis@users.noreply.github.com>
2022-04-10 20:11:30 -0700

    Add files via upload

---
+++ sketch - Copy.js
@@ -0,0 +1,42 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+
+let engine;
+let world;
+var ground;
+
+function setup() 
+{
+  createCanvas(500,700);
+  frameRate(80);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(200,680,600,20);
+  rope = new Rope(5,{x:200,y:10})
+  fruit = Bodies.circle(200,300,50)
+  Composite.add(rope.body,fruit)
+  constraint = new Link(rope, fruit)
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+  textSize(50)
+  
+}
+
+function draw() 
+{
+  background(51);
+  ground.show();
+  rope.show();
+  ellipse(fruit.position.x,fruit.position.y,30,30)
+  Engine.update(engine);
+  
+
+ 
+   
+}
